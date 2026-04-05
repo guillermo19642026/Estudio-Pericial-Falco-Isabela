497581b7a575379ec80a452b651b59ad2469e48a
guillermo19642026 <guillermohechem2019@gmail.com>
2026-04-05 11:28:33 -0300

    Menu hamburguesa mobile

--- script.js
+++ script.js
@@ -657,3 +657,15 @@ function toggleRecurso(el){
 }
 
 
+
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("active");
+}
+
+
+document.querySelectorAll("#menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    document.getElementById("menu").classList.remove("active");
+  });
+});
