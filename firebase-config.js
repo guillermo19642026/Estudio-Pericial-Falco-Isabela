888fe007f8c06ca7a886d50c339fd48598212de7
guillermo19642026 <guillermohechem2019@gmail.com>
2026-06-20 11:09:16 -0300

    carga de dni y certificados

--- firebase-config.js
+++ firebase-config.js
@@ -3,8 +3,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-// TU CONFIG REAL (la que copiaste)
 const firebaseConfig = {
   apiKey: "AIzaSyDrWRqbgWPNXb3IQP0S7YKC1zuS5DmtWj8",
   authDomain: "tests-psicologicos.firebaseapp.com",
@@ -14,12 +14,10 @@ const firebaseConfig = {
   appId: "1:985283051227:web:f284cdfaa970cbeb64d6fe"
 };
 
-// Inicializar
 const app = initializeApp(firebaseConfig);
 
-// Servicios
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-// Exportar
-export { auth, db };
+export { auth, db, storage };
